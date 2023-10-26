52bb54dd7150ed47126052410729499ee665d52a
Anmol Sharma <95350969+anmol09000@users.noreply.github.com>
2023-10-27 01:00:02 +0530

    Add files via upload

---
+++ authService.js
@@ -0,0 +1,18 @@
+let keyName="user";
+function login(obj){
+    let str = JSON.stringify(obj);
+    localStorage.setItem(keyName,str);
+}
+function logout(){
+    localStorage.removeItem(keyName);
+}
+function getUser(){
+    let str = localStorage.getItem(keyName);
+    let obj= JSON.parse(str);
+    return obj;
+}
+export default {
+    login,
+    logout,
+    getUser,
+};
